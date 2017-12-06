f2873872b9506ca871d92491e0c771e5200ef8f4
clark he <arcaneade@gmail.com>
2017-12-06 06:20:58 +0000

    finish home style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,12 +2,19 @@
  <html>
  <head>
     <title>@yield('title','Sample APP')</title>
+     <link rel="stylesheet" href="/css/app.css">
  </head>
  <body>
-    <header class="nav navbar-fixed-top navbar-right">
+    <header class="nav navbar-fixed-top navbar-inverse">
         <div class="container">
-            <div>
-              <a href="/" id="logo">Sample App</a>
+            <div class="col-md-offset-1 col-md-10">
+                <a href="/" id="logo">Sample App</a>
+                <nav>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/help">Help Page</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </header>
--- resources/views/static-pages/home.blade.php
+++ resources/views/static-pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends ('layouts.default')
-@section('title','Home')
+
 @section('content')
-    <h1>Home Page</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            What you are watching is <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Larave</a>
+        </p>
+        <p>
+            Everthing starts from here
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">Enroll Now</a>
+        </p>
+    </div>
 @stop
